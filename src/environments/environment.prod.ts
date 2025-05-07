c1dca03cc9a60a928751572a6f969fe86d365291
Seifeddine ABIDI <seifeddine.abidi@esprit.tn>
2025-05-07 15:57:44 +0100

    env changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080',
+    apiUrl: 'http://approbation-backend:8080',
     recaptcha: {
         siteKey: '6LdDuhcrAAAAAHrytAMLMiwP_PGlS7y_cAKQMqHn',
       },
