8b25b9699e4c43052d375aa7b1a08b7bb1918c00
Seifeddine ABIDI <seifeddine.abidi@esprit.tn>
2025-05-12 17:04:03 +0100

    replaced env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://approbation-backend:8080',
+    apiUrl: 'http://backend.192.168.2.189.nip.io',
     recaptcha: {
         siteKey: '6LdDuhcrAAAAAHrytAMLMiwP_PGlS7y_cAKQMqHn',
       },
